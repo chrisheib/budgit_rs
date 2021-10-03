283c0cd94ce6d08e855eaf68ba4073fc6dd7445c
STSchiff <chrisheib@gmx.de>
2021-10-03 21:28:48 +0200

    Improve DB to Vec and theme

--- src/data.rs
+++ src/data.rs
@@ -1,38 +1,28 @@
-use actix_web::{error::ErrorInternalServerError, Error, Result};
-use liquid::{object, Object};
+use actix_web::{Error, Result};
+use liquid::{
+    model::{value, Value},
+    object, Object,
+};
 
-use crate::db::adb_con;
+use crate::db::adb_query_vec;
 
-pub fn get_all_main_categories() -> Result<Object, Error> {
-    let s = "SELECT * FROM hauptkat";
-    let c = adb_con()?;
-    let mut query = c.prepare(s).map_err(ErrorInternalServerError)?;
-    let mut rows = query.query([]).map_err(ErrorInternalServerError)?;
-
-    let mut a = Vec::<Object>::new();
-    while let Some(r) = rows.next().map_err(ErrorInternalServerError)? {
+pub fn get_all_main_categories() -> Result<Value, Error> {
+    let sql = "SELECT * FROM hauptkat";
+    let a: Vec<Object> = adb_query_vec(sql, [], |r| {
         let name: String = r.get("name").unwrap();
         let id = r.get::<_, u32>("id").unwrap();
         let id = id.to_string();
-        let subcat = get_sub_categories(&id)?;
-        let cat_obj = object!({ "name": name, "id": id , "sc": subcat});
-        a.push(cat_obj);
-    }
-    Ok(object!({ "kats": &a[..] }))
+        let subcat = get_sub_categories(&id).unwrap();
+        object!({ "name": name, "id": id , "sc": subcat})
+    })?;
+    Ok(value!(&a[..]))
 }
 
-pub fn get_sub_categories(main_cat: &str) -> Result<Object, Error> {
-    let s = "SELECT * FROM unterkat WHERE hauptkatid = ?";
-    let c = adb_con()?;
-    let mut query = c.prepare(s).map_err(ErrorInternalServerError)?;
-    let mut rows = query.query([main_cat]).map_err(ErrorInternalServerError)?;
-
-    let mut a = Vec::<Object>::new();
-
-    while let Some(r) = rows.next().map_err(ErrorInternalServerError)? {
+pub fn get_sub_categories(main_cat: &str) -> Result<Value, Error> {
+    let sql = "SELECT * FROM unterkat WHERE hauptkatid = ?";
+    let a: Vec<Object> = adb_query_vec(sql, [main_cat], |r| {
         let name: String = r.get("name").unwrap();
-        let sub_obj = object!({ "name": name });
-        a.push(sub_obj);
-    }
-    Ok(object!({ "subkats": &a[..] }))
+        object!({ "name": name })
+    })?;
+    Ok(value!(&a[..]))
 }
--- src/db.rs
+++ src/db.rs
@@ -1,8 +1,9 @@
 use crate::errconv;
-use actix_web::{error::ErrorInternalServerError, Error};
-use rusqlite::{Connection, Params, Rows};
+use actix_web::Error;
+use rusqlite::{Connection, Params};
 use stable_eyre::{eyre::Context, Result};
 
+#[allow(dead_code)]
 pub fn adb_select<T, P, F>(sql: &str, params: P, f: F) -> Result<T, Error>
 where
     P: Params,
@@ -17,26 +18,54 @@ where
     F: FnOnce(&rusqlite::Row<'_>) -> std::result::Result<T, rusqlite::Error>,
 {
     let c = db_con()?;
-    let mut stmt = c.prepare(&sql)?;
+    let mut stmt = c.prepare(sql)?;
     stmt.query_row(params, f).wrap_err("query_row")
 }
 
+pub fn db_query_vec<T, P, F>(sql: &str, params: P, f: F) -> Result<Vec<T>>
+where
+    P: Params,
+    F: Fn(&rusqlite::Row<'_>) -> T,
+{
+    let c = db_con()?;
+    let mut query = c.prepare(sql)?;
+    let mut rows = query.query(params)?;
+
+    let mut a = Vec::<T>::new();
+    while let Some(r) = rows.next()? {
+        a.push(f(r))
+    }
+    Ok(a)
+}
+
+pub fn adb_query_vec<T, P, F>(sql: &str, params: P, f: F) -> actix_web::Result<Vec<T>, Error>
+where
+    P: Params,
+    F: Fn(&rusqlite::Row<'_>) -> T,
+{
+    errconv(db_query_vec(sql, params, f))
+}
+
+#[allow(dead_code)]
 fn db_uint32_read(sql: &str) -> Result<u32> {
     let c = db_con()?;
     c.query_row::<u32, _, _>(sql, [], |row| row.get(0))
         .wrap_err(format!("db_uint32_read: {}", sql))
 }
 
+#[allow(dead_code)]
 pub fn adb_uint32_read(sql: &str) -> actix_web::Result<u32, Error> {
     errconv(db_uint32_read(sql))
 }
 
+#[allow(dead_code)]
 fn db_str_read(sql: &str) -> Result<String> {
     let c = db_con()?;
     c.query_row::<String, _, _>(sql, [], |row| row.get(0))
         .wrap_err(format!("db_str_read: {}", sql))
 }
 
+#[allow(dead_code)]
 pub fn adb_str_read(sql: &str) -> actix_web::Result<String, Error> {
     errconv(db_str_read(sql))
 }
